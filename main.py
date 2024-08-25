25a8e39b05d68017bd2346f620f3da64c2360653
styxofsyntax <107263899+styxofsyntax@users.noreply.github.com>
2024-08-25 05:24:54 +0500

    Add tracking to a moving ball
    - Ball moves horizontally at a given rate

--- main.py
+++ main.py
@@ -2,33 +2,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def mouse_move(event):
-    x, y = event.xdata, event.ydata
-    update_plot(x, y)
+def ball_move(rate):
+    ball_coords[0] += rate
+    update_plot(ball_coords)
 
 
-def update_plot(U, V):
-    try:
-        quiver.set_UVC(-X + U, -Y + V)
-        plt.draw()
-    except:
-        pass
+def update_plot(UV):
+    U, V = UV
+    quiver.set_UVC(-X + U, -Y + V)
+    plt.draw()
 
 
+SIZE = 20
 plt.style.use('_mpl-gallery-nogrid')
-x = np.linspace(-4, 4, 10)
-y = np.linspace(-4, 4, 10)
+x = np.linspace(-SIZE, SIZE, 10)
+y = np.linspace(-SIZE, SIZE, 10)
 X, Y = np.meshgrid(x, y)
 
-U = 1
-V = 1
+ball_coords = [-SIZE, 0]
 
-fig, ax = plt.subplots()
-quiver = ax.quiver(X, Y, U, V, color="lightcoral", angles='xy',
-                   scale_units='xy', scale=5, width=.005)
-ax.set(xlim=(-5, 5), ylim=(-5, 5))
+fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+quiver = ax.quiver(X, Y, ball_coords[0], ball_coords[1], color="lightcoral",
+                   angles='xy', scale_units='xy', scale=5, width=.005)
+ax.set(xlim=(-(SIZE+1), SIZE+1), ylim=(-(SIZE+1), SIZE+1))
 ax.format_coord = lambda x, y: ""
 
-plt.connect('motion_notify_event', mouse_move)
+ball = plt.Circle(ball_coords, 0.3, color='cornflowerblue')
+ax.add_patch(ball)
 plt.axis('off')
+
+rate = 0.1
+for i in np.arange(0, SIZE*2, rate):
+    ball_move(rate)
+    plt.pause(0.01)
+
 plt.show()
