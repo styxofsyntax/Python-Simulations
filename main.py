6eb25abc6d8d1db53d9f8d91ba20f244a63dc229
styxofsyntax <107263899+styxofsyntax@users.noreply.github.com>
2024-08-25 20:27:22 +0500

    Restore original main.py

--- main.py
+++ main.py
@@ -2,38 +2,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def ball_move(rate):
-    ball_coords[0] += rate
-    update_plot(ball_coords)
+def mouse_move(event):
+    x, y = event.xdata, event.ydata
+    update_plot(x, y)
 
 
-def update_plot(UV):
-    U, V = UV
-    quiver.set_UVC(-X + U, -Y + V)
-    plt.draw()
+def update_plot(U, V):
+    try:
+        quiver.set_UVC(-X + U, -Y + V)
+        plt.draw()
+    except:
+        pass
 
 
-SIZE = 20
 plt.style.use('_mpl-gallery-nogrid')
-x = np.linspace(-SIZE, SIZE, 10)
-y = np.linspace(-SIZE, SIZE, 10)
+x = np.linspace(-4, 4, 10)
+y = np.linspace(-4, 4, 10)
 X, Y = np.meshgrid(x, y)
 
-ball_coords = [-SIZE, 0]
+U = 1
+V = 1
 
-fig, ax = plt.subplots(1, 1, figsize=(5, 5))
-quiver = ax.quiver(X, Y, ball_coords[0], ball_coords[1], color="lightcoral",
-                   angles='xy', scale_units='xy', scale=5, width=.005)
-ax.set(xlim=(-(SIZE+1), SIZE+1), ylim=(-(SIZE+1), SIZE+1))
+fig, ax = plt.subplots()
+quiver = ax.quiver(X, Y, U, V, color="lightcoral", angles='xy',
+                   scale_units='xy', scale=5, width=.005)
+ax.set(xlim=(-5, 5), ylim=(-5, 5))
 ax.format_coord = lambda x, y: ""
 
-ball = plt.Circle(ball_coords, 0.3, color='cornflowerblue')
-ax.add_patch(ball)
+plt.connect('motion_notify_event', mouse_move)
 plt.axis('off')
-
-rate = 0.1
-for i in np.arange(0, SIZE*2, rate):
-    ball_move(rate)
-    plt.pause(0.01)
-
 plt.show()
